cb3837f6e2fd5cf8122ecb9466b4c7bb6ad2ff35
azalen <bastiari@yahoo.de>
2021-09-08 08:38:23 -0700

    0808:1

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,20 @@
+from blog.models import Post
 from django.urls import path
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+    # looks for <app>/<model>_<viewtype>.html but we only have home.html
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('about/', views.about, name='blog-about'),
+    
+    # We need URL for each blogpost
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 ]
 
